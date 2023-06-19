c0745c29ff4058090328c84d79e8adc72a57a0b1
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-06-18 20:45:49 -0500

    Chapter 5

--- script.js
+++ script.js
@@ -5,99 +5,138 @@ const BALLZ = [];
 
 let LEFT, UP, RIGHT, DOWN;
 let friction = 0.1;
-class Ball {
-    constructor(x, y, r) {
+
+//a class Vector with basic vector operations
+class Vector{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    add(v){
+        return new Vector(this.x+v.x, this.y+v.y);
+    }
+
+    subtr(v){
+        return new Vector(this.x-v.x, this.y-v.y);
+    }
+
+    mag(){
+        return Math.sqrt(this.x**2 + this.y**2)
+    }
+
+    mult(n){
+        return new Vector(this.x*n, this.y*n);
+    }
+
+    drawVec(start_x, start_y, n, color){
+        ctx.beginPath();
+        ctx.moveTo(start_x, start_y);
+        ctx.lineTo(start_x + this.x * n, start_y + this.y * n);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
+
+class Ball{
+    constructor(x, y, r){
         this.x = x;
         this.y = y;
         this.r = r;
-        this.vel_x = 0;
-        this.vel_y = 0;
-        this.acc_x = 0;
-        this.acc_y = 0;
+        this.vel = new Vector(0,0);
+        this.acc = new Vector(0,0);
         this.acceleration = 1;
         this.player = false;
         BALLZ.push(this);
     }
-    drawBall() {
+
+    drawBall(){
         ctx.beginPath();
-        ctx.arc(this.x, this.y,this.r,0,2*Math.PI);
-        ctx.strokeStyle = 'black';
+        ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+        ctx.strokeStyle = "black";
         ctx.stroke();
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = "red";
         ctx.fill();
+        ctx.closePath();
     }
-    display() {
-        ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x + this.acc_x * 100, this.y + this.acc_y * 100);
-        ctx.strokeStyle = 'green';
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x + this.vel_x * 10, this.y + this.vel_y * 10);
-        ctx.strokeStyle = 'blue';
-        ctx.stroke();
+
+    display(){
+        this.vel.drawVec(this.x, this.y, 10, "green");
+        this.acc.drawVec(this.x, this.y, 100, "blue");
     }
 }
 
-function keyControl(b) {
-    window.addEventListener('keydown', (e) => {
-        //console.log(e.key);
-        if (e.keyCode === 37) {
+function keyControl(b){
+    canvas.addEventListener('keydown', function(e){
+        if(e.keyCode === 37){
             LEFT = true;
-        } else if (e.keyCode === 38) {
+        }
+        if(e.keyCode === 38){
             UP = true;
-        } else if (e.keyCode === 39) {
+        }
+        if(e.keyCode === 39){
             RIGHT = true;
-        } else if (e.keyCode === 40) {
+        }
+        if(e.keyCode === 40){
             DOWN = true;
         }
-    })
-    window.addEventListener('keyup', (e) => {
-        //console.log(e.key);
-        if (e.keyCode === 37) {
+    });
+    
+    canvas.addEventListener('keyup', function(e){
+        if(e.keyCode === 37){
             LEFT = false;
-        } else if (e.keyCode === 38) {
+        }
+        if(e.keyCode === 38){
             UP = false;
-        } else if (e.keyCode === 39) {
+        }
+        if(e.keyCode === 39){
             RIGHT = false;
-        } else if (e.keyCode === 40) {
+        }
+        if(e.keyCode === 40){
             DOWN = false;
         }
-    })
-    if (LEFT) b.acc_x = -b.acceleration;
-    if (UP) b.acc_y = -b.acceleration;
-    if (RIGHT) b.acc_x = b.acceleration;
-    if (DOWN) b.acc_y = b.acceleration;
-    if (!UP && !DOWN){
-        b.acc_y = 0;
+    });
+    
+    if(LEFT){
+        b.acc.x = -b.acceleration;
     }
-    if (!RIGHT && !LEFT) {
-        b.acc_x = 0;
+    if(UP){
+        b.acc.y = -b.acceleration;
     }
-    b.vel_x += b.acc_x;
-    b.vel_y += b.acc_y;
-    b.vel_x *= 1-friction;
-    b.vel_y *= 1 - friction;
-    b.x += b.vel_x;
-    b.y += b.vel_y;
+    if(RIGHT){
+        b.acc.x = b.acceleration;
+    }
+    if(DOWN){
+        b.acc.y = b.acceleration;
+    }
+    if(!LEFT && !RIGHT){
+        b.acc.x = 0;
+    }
+    if(!UP && !DOWN){
+        b.acc.y = 0;
+    }
+    
+    //acceleration vector gets added to the velocity vector
+    b.vel = b.vel.add(b.acc);
+    b.vel = b.vel.mult(1-friction);
+    b.x += b.vel.x;
+    b.y += b.vel.y;
 }
 
-
-function mainLoop() {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    BALLZ.forEach(b => {
+function mainLoop(timestamp) {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    BALLZ.forEach((b) => {
         b.drawBall();
-        if (b.player === true) {
+        if (b.player){
             keyControl(b);
         }
         b.display();
     })
     requestAnimationFrame(mainLoop);
-};
-let Ball1 = new Ball(200,200,30);
-let Ball2 = new Ball(300,300, 20);
-Ball1.player = true;
-Ball2.player = true;
-mainLoop();
+}
 
+let Ball1 = new Ball(200, 200, 30);
+Ball1.player = true;
+
+requestAnimationFrame(mainLoop);
